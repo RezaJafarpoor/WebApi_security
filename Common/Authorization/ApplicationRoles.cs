77494f62a7be92817a69336e17cc4c15937ac40b
reza <rezajafarpoor222@gmail.com>
2024-06-29 20:22:28 +0330

    adding application roles

---
+++ Common/Authorization/ApplicationRoles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Common.Authorization;
+
+public static class ApplicationRoles
+{
+    public const string Admin = nameof(Admin);
+    public const string BasicUser = nameof(BasicUser);
+
+    public static IReadOnlyList<string> DefaultRoles { get; } = new ReadOnlyCollection<string>(new []
+    {
+        Admin,
+        BasicUser
+    });
+
+    public static bool IsDefault(string roleName) => DefaultRoles.Any(x => x == roleName);
+
+}
